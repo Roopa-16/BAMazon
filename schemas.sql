2e2158fc3abc8f00f261825007a286ff497d57f6
Roopa <roopatel@valdosta.edu>
2019-08-22 16:46:26 -0400

    stupid m instead of a n

--- schemas.sql
+++ schemas.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS bamazom_DB;
 CREATE database bamazon_DB;
 USE bamazon_DB;
-CREATE TABLE products(
+CREATE TABLE productsamazon (
 	item_id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
@@ -9,19 +9,18 @@ CREATE TABLE products(
     stock_quantity INT(10) NOT NULL,
     primary key(item_id)
 );
-INSERT INTO products(product_name,department_name,price,stock_quantity)
-VALUES("Bananas","Grocery",01.25,150),
+INSERT INTO productsamazon(product_name,department_name,price,stock_quantity)
+VALUES ("Bananas","Grocery",01.25,150),
 	("Cucumbers","Grocery",01.39,50),
     ("Carrots","Grocery",03.49,200),
     ("Demin Jeans","Clothing",39.99,15),
     ("Ripped Jeans","Clothing",54.25,35),
     ("Athletic T-Shirt","Clothing",20.00,42),
-    ("MacBook Pro 15 inch","Technology",2,900.99,25),
-    ("IPhone XR","Technology",749.00,50),
+    ("MacBook Pro 15 inch","Technology",2900.99,25),
+    ("IPhone XR","Technology",649.00,50),
     ("Apple Watch","Technology",849.00,35),
     ("Jenga","Entertainment",12.99,20),
     ("Sequence","Entertainment",20.00,15),
     ("Monopoly","Entertainment",15.00,36);
 SELECT * FROM bamazon_DB;
-SELECT * FROM products;
-    
+SELECT * FROM productsamazon;
